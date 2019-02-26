2c6568acef8e0b658ad751ea1f1f632b66b541bc
willbasky <vladislav.sabanov@gmail.com>
2019-02-26 18:59:25 +0500

    Upgrade to tidal-1.0.7 version.
    Added comments.
    Added howto run animated pattern.
    Refactored all code.
    Upraded to 13.08 lts.
    Added 'stack.yml'
    Improved '.cabal' config

---
+++ src/Examples.hs
@@ -0,0 +1,108 @@
+module Examples where
+
+import Data.Colour
+import Sound.Tidal.Context
+
+import Common (dirtToColour)
+import Vis
+import VisCycle
+import VisGradient
+
+
+-- | Examples
+--
+-- | For  pattern animation look at 'runAnimation' function at 'CycleAnimation.hs' module.
+-- | There two forms of moving patterns: cycle and rectangle.
+--
+-- | Here is renders of still images only.
+main :: IO ()
+main = do
+  renderMatBundlePDF "./examples/" [a, b, c, d, e, f, g]
+  return ()
+
+-- | Make mat rectangle pattern
+matRect :: IO ()
+matRect = renderMatPDF "./examples/matRect" pip
+
+-- | Make bundle of mat rectangle pattern
+matBundleRect :: IO ()
+matBundleRect = renderMatBundlePDF "./examples/" [foo, pip, pop, bar, buz]
+
+-- | Make gradient rectangle pattern
+gradientRect :: IO ()
+gradientRect = renderGradientPDF "./examples/gradientRect" pip
+
+-- | Make gradient rectangle pattern
+matCycleWithBorders :: IO ()
+matCycleWithBorders = renderCyclePDF "./examples/cycle" "background text" pip
+
+-- | Prepared patterns.
+foo :: Pattern ColourD
+foo = dirtToColour $ striate 16 $ sound "[bd*3? dr2, ~ casio ~, [bd arpy]]" # n
+  "2? 3 1 2"
+
+pip :: Pattern ColourD
+pip = dirtToColour $ fast 12 $ sound
+  "[bd bd bd, <[sd sd] cp>, <arpy [arpy <[arpy arpy]> arpy arpy]>, odx]"
+
+pop :: Pattern ColourD
+pop = dirtToColour $ fast 12 $ loopAt 3 $ sound
+  "[~ bd bd ~] ~ [bd ~ ~ [sd ~ ~ sd] ~ ~ sd]"
+
+bar :: Pattern ColourD
+bar = dirtToColour $ fast 12 $ sound "{~ ~ ~ ~, arpy bass2 drum notes can}"
+
+buz :: Pattern ColourD
+buz =
+  dirtToColour $ fast 24 $ sound "arpy*4" # pan (range 0.25 0.75 sine) # gain
+    (range 1.2 0.5 sine)
+
+a :: Pattern ColourD
+a = density 16 $ every 2 rev $ every 3 (superimpose (iter 4)) $ rev
+  "[black blue darkblue, grey lightblue]"
+
+b :: Pattern (Colour Double)
+b = flip darken <$> "[black blue orange, red green]*16" <*> sine
+
+c :: Pattern (Colour Double)
+c =
+  density 10
+    $   flip darken
+    <$> "[black blue, grey ~ navy, cornflowerblue blue]*2"
+    <*> (slow 5 $ (*) <$> sine <*> (slow 2 tri))
+
+d :: Pattern (Colour Double)
+d =
+  every 2 rev
+    $ density 10
+    $ (   blend'
+      <$> "blue navy"
+      <*> "orange [red, orange, purple]"
+      <*> (slow 6 $ sine)
+      )
+  where blend' x y z = blend z x y
+
+e :: Pattern (Colour Double)
+e =
+  density 32
+    $   flip over
+    <$> "[grey olive, black ~ brown, darkgrey]"
+    <*> (   withOpacity
+        <$> "[beige, lightblue white darkgreen, beige]"
+        <*> ((*) <$> (slow 8 $ slow 4 sine) <*> (slow 3 $ sine))
+        )
+
+f :: Pattern ColourD
+f =
+  density 2
+    $   flip darken
+    <$> (density 8 $ "[black blue, grey ~ navy, cornflowerblue blue]*2")
+    <*> sine
+
+g :: Pattern ColourD
+g = density 2 $ do
+  let x = "[skyblue olive, grey ~ navy, cornflowerblue green]"
+  coloura <- density 8 x
+  colourb <- density 4 x
+  slide'  <- slow 2 sine
+  return $ blend slide' coloura colourb
